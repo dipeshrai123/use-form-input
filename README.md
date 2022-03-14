29cf9e898bcdcb97d6958e01c03d12f8c6404b36
Dipesh Rai <raidipesh78@gmail.com>
2022-03-14 18:54:20 +0545

    useFormInput hook init + gitignore

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Package Config
-> Config file for package development 
+# USE-FORM-INPUT
+
+> Simple hook to provide form validation in react
+
+### Install
+
+Install with npm:
+
+```sh
+npm i use-form-input
+```
+
+or
+Install with yarn:
+
+```sh
+yarn add use-form-input
+```
+
+## License
+
+MIT
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,99 @@
-console.log("HELLO CONFIG");
+import * as React from "react";
+
+type ErrorType<T> = {
+  [key in keyof T]: { error: boolean; messages: Array<string> };
+};
+
+type ValueType = object | [] | string | boolean | number;
+
+type ValidatorConfigType = {
+  condition: boolean;
+  message?: string;
+  onMatched?: (messages?: string) => void;
+};
+
+type ValidatorType<T> = (
+  errors: any
+) => (name: keyof T, config: ValidatorConfigType) => void;
+
+export function useFormInput<T>(fields: T): [
+  T,
+  {
+    onChange: (
+      name: keyof T,
+      value?: ValueType | ((previousValue: ValueType) => any)
+    ) => (event?: any) => void;
+    validator: ValidatorType<T>;
+    isValid: (errors: ErrorType<T> | {}) => boolean;
+    errors: ErrorType<T>;
+    setErrors: React.Dispatch<React.SetStateAction<{}>>;
+  }
+] {
+  const [data, setData] = React.useState<T>({
+    ...fields,
+  });
+  const [errors, setErrors] = React.useState<any>({});
+
+  const onChange = (
+    name: keyof T,
+    value?: ValueType | ((previousValue: ValueType) => any)
+  ) => {
+    return function (event?: any) {
+      setData((prev) => {
+        if (value === undefined || value === null) {
+          return { ...prev, [name]: event?.target?.value };
+        } else {
+          if (typeof value === "function") {
+            const functionReturnValue = value(prev[name] as any);
+            return { ...prev, [name]: functionReturnValue };
+          } else {
+            return { ...prev, [name]: value };
+          }
+        }
+      });
+    };
+  };
+
+  const validator = (errors: any) => {
+    return function (name: keyof T, config: ValidatorConfigType) {
+      const { condition, message, onMatched } = config;
+
+      if (condition) {
+        errors[name] = {
+          error: true,
+          messages: errors[name]?.messages ?? [],
+        };
+
+        const hasErrorMessage = message !== null && message !== undefined;
+
+        if (Object.prototype.hasOwnProperty.call(errors, name)) {
+          if (hasErrorMessage) {
+            errors[name].messages = [...errors[name].messages, message];
+          }
+        } else {
+          if (hasErrorMessage) {
+            errors[name].messages = [message];
+          }
+        }
+
+        if (onMatched) {
+          onMatched(message);
+        }
+      } else {
+        delete errors[name];
+      }
+    };
+  };
+
+  const isValid = (errors: ErrorType<T> | {}) => {
+    return Object.keys(errors).length === 0;
+  };
+
+  return [
+    data,
+    React.useMemo(
+      () => ({ onChange, validator, isValid, errors, setErrors }),
+      [errors]
+    ),
+  ];
+}
